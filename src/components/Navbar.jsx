bc30194bab4fe87863d0168f4f01f853ba09430b
SalasDelil <salasdelil8@gmail.com>
2023-06-27 22:56:08 +0300

    navbar update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,103 @@
-import { AppBar, Toolbar, styled, Typography, InputBase } from "@mui/material";
-import { MenuIcon } from "@mui/icons-material";
+import {
+  AppBar,
+  Toolbar,
+  styled,
+  Typography,
+  InputBase,
+  Box,
+  Badge,
+  Avatar,
+  Menu,
+  MenuItem,
+} from "@mui/material";
+import { Mail, Notifications, Widgets } from "@mui/icons-material";
+import { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 
 const Navbar = () => {
+  const [open, setOpen] = useState(false)
+
   return (
-    <AppBar>Navbar</AppBar>
-  )
+    <AppBar position="stick">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Navbar
+        </Typography>
+        <Widgets sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={3} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="./images/sali.jpg"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="./images/sali.jpg"
+          />
+          <Typography variant="span">Salas</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="positioned-menu"
+        aria-labelledby="positioned-button"
+        onClose={(e) => setOpen(false)}
+        open={open}
+        anchorOrigin={{
+          vertical: "top",
+          horizontal: "right"
+        }}
+        transformOrigin={{
+          vertical:"top",
+          horizontal:"right"
+        }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+
+      </Menu>
+    </AppBar>
+  );
 };
 
 export default Navbar;
