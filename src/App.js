6bde46d6be0cad5afdc1c765ec1997eec01d943e
SalasDelil <salasdelil8@gmail.com>
2023-06-29 23:24:29 +0300

    Added darktheme

--- src/App.js
+++ src/App.js
@@ -2,18 +2,29 @@ import Sidebar from "./components/Sidebar";
 import Rightbar from "./components/Rightbar";
 import Feed from "./components/Feed";
 import Navbar from "./components/Navbar";
-import { Box, Stack } from "@mui/material"
+import { Box, Stack, ThemeProvider, createTheme } from "@mui/material";
+import { useState } from "react";
 
 function App() {
+
+  const [mode, setMode] = useState("light")
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: setMode("dark")
+    }
+  })
   return (
-    <Box>
-      <Navbar />
-      <Stack direction="row" spacing={2} justifyContent="space-between">
-        <Sidebar />
-        <Feed />
-        <Rightbar />
-      </Stack>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color="primary">
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+      </Box>
+    </ThemeProvider>
   );
 }
 
